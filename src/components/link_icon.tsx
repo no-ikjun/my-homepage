458004a48d24d86d026d3f9f9ff4fffa9a42d451
ikjunchoi <choiikjun1101@gmail.com>
2023-12-15 02:20:26 +0900

    ✨ feat: main page ui

---
+++ src/components/link_icon.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import styles from "../app/page.module.css";
+import Image from "next/image";
+
+export enum LinkType {
+  Github,
+  LinkedIn,
+  Instagram,
+  Mail,
+}
+
+export default function LinkIcon({ linkType }: { linkType: LinkType }) {
+  let href = "";
+  let src = "";
+  let alt = "";
+  switch (linkType) {
+    case LinkType.Github:
+      href = "https://github.com/no-ikjun";
+      src = "./img/github_icon.svg";
+      alt = "Github";
+      break;
+    case LinkType.LinkedIn:
+      href = "https://www.linkedin.com/in/ikjunchoi/";
+      src = "./img/linkedin_icon.svg";
+      alt = "LinkedIn";
+      break;
+    case LinkType.Instagram:
+      href = "https://www.instagram.com/hi_ikjun/";
+      src = "./img/instagram_icon.svg";
+      alt = "Instagram";
+      break;
+    case LinkType.Mail:
+      href = "mailto:choiikjun1101@gmail.com";
+      src = "./img/mail_icon.svg";
+      alt = "Mail";
+      break;
+  }
+  return (
+    <div className={styles.icon_container}>
+      <Link href={href}>
+        <Image
+          className={styles.icon}
+          src={src}
+          alt={alt}
+          width={32}
+          height={32}
+        />
+      </Link>
+    </div>
+  );
+}
