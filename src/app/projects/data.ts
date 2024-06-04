08791f56fd286fe83605835d084fe313dcfd7383
ikjunchoi <choiikjun1101@gmail.com>
2024-06-04 23:35:16 +0900

    feat: add project page

---
+++ src/app/projects/data.ts
@@ -0,0 +1,44 @@
+// pages/projects/data.js
+export const projects = [
+  {
+    title: "SPRIT",
+    description: "App service that creates a steady reading habit.",
+    directLink: "https://sprit-app.me",
+    techStack: ["flutter", "NextJS", "NestJS", "AWS"],
+    links: [
+      {
+        type: "github",
+        url: "https://github.com/no-ikjun/sprit",
+        description: "APP GitHub repository",
+      },
+      {
+        type: "github",
+        url: "https://github.com/no-ikjun/sprit-be",
+        description: "Backend GitHub repository",
+      },
+      {
+        type: "website",
+        url: "https://disquiet.io/product/%EC%8A%A4%ED%94%84%EB%A6%BF",
+        description: "Disquiet project url",
+      },
+    ],
+  },
+  {
+    title: "AI Analyst",
+    description: "Auto-reporting system for stock item analysis using ChatGPT",
+    directLink: "https://analyst.ikjun.com",
+    techStack: ["React", "CloudFlare", "NestJS", "AWS"],
+    links: [
+      {
+        type: "github",
+        url: "https://github.com/no-ikjun/analyst-nest",
+        description: "Backend GitHub repository",
+      },
+      {
+        type: "github",
+        url: "https://github.com/no-ikjun/analyst-fe",
+        description: "Frontend GitHub repository",
+      },
+    ],
+  },
+];
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,33 @@
 import styles from "./page.module.css";
+import ProjectBox from "@/components/project_box";
+import { projects } from "./data";
+import Link from "next/link";
 
-export default function ProjectPage() {
+export default function ProjectsPage() {
   return (
     <div className={styles.main}>
-      <h1>Project Page</h1>
+      <div className={styles.container}>
+        <Link href="/" className={styles.back_button}>
+          <p className={styles.left_arrow}>&larr;</p>
+        </Link>
+        <div className={styles.title_container}>
+          <h1 className={styles.title}>Projects</h1>
+          <p className={styles.description}>My Own Projects Since 2023</p>
+          <p className={styles.description}>Updated : 2024.06</p>
+        </div>
+        <div className={styles.content_container}>
+          {projects.map((project) => (
+            <ProjectBox
+              title={project.title}
+              description={project.description}
+              directLink={project.directLink}
+              techStack={project.techStack}
+              links={project.links}
+              key={project.title}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
